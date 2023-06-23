9aefe5a5951f1800f6db11fb2204cd1fa9a1312f
Aquileo León <leon.automatizacion@gmail.com>
2023-06-23 12:50:04 -0600

    Se centro Spinner y se termino proyecto

--- criptomonedas-react/src/components/Spinner.jsx
+++ criptomonedas-react/src/components/Spinner.jsx
@@ -2,14 +2,14 @@ import '../styles/Spinner.css'
 
 const Spinner = () => {
   return (
-    <div className="sk-chase">
+    <div className="sk-chase" >
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
-    </div>
+      </div>  
   )
 }
 
